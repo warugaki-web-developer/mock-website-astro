e73ae7eff673389f6429af1075b385777ab92486
warugaki <warugaki.web.developer@gmail.com>
2023-07-10 00:05:19 +0900

    add: Stylelintのインストールと設定

---
+++ .stylelintrc.cjs
@@ -0,0 +1,11 @@
+/** @type {import("stylelint").Config} */
+module.exports = {
+  extends: [
+    'stylelint-config-html',
+    'stylelint-config-html/astro',
+    'stylelint-config-standard',
+    'stylelint-rscss/config',
+    'stylelint-config-recess-order',
+  ],
+  rules: {},
+};
