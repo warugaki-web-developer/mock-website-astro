1be4e3ebe532e47ecb55ab037c5de0f4f077c62d
warugaki <warugaki.web.developer@gmail.com>
2023-07-10 22:34:48 +0900

    add: postcss(postcss-nested)
    - sassと同じようにネストで記述できるようにするため

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const autoprefixer = require('autoprefixer');
+const postcssNested = require('postcss-nested');
 
 module.exports = {
-  plugins: [autoprefixer()],
+  plugins: [postcssNested, autoprefixer()],
 };
